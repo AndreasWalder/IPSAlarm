9155f7a1df10529411993711989ab52de2029232
Andreas Walder <andreas.walder@web.de>
2019-05-20 15:53:35 +0200

    .

--- IPSAlarm/module.php
+++ IPSAlarm/module.php
@@ -7,7 +7,7 @@
             //Never delete this line!
             parent::Create();
 			
-			$this->RegisterPropertyString("AlexaDevices", "");
+			$this->RegisterPropertyString("TreeData", "");
             
 			$this->RegisterPropertyInteger("Duration", 1);
             $this->RegisterPropertyInteger("OutputID", 0);
@@ -135,22 +135,23 @@
 			$data = json_decode(file_get_contents(__DIR__ . "/form.json"));
 			
 			//Only add default element if we do not have anything in persistence
-			if($this->ReadPropertyString("AlexaDevices") == "") {			
+			if($this->ReadPropertyString("TreeData") == "") {			
 				$data->elements[0]->values[] = Array(
 					"instanceID" => 12435,
 					"name" => "ABCD",
 					"state" => "OK!",
+					"temperature" => 23.31,
 					"rowColor" => "#ff0000"
 				);
 			} else {
 				//Annotate existing elements
-				$AlexaDevices = json_decode($this->ReadPropertyString("AlexaDevices"));
-				foreach($AlexaDevices as $AlexaRow) {
+				$treeData = json_decode($this->ReadPropertyString("TreeData"));
+				foreach($treeData as $treeRow) {
 					//We only need to add annotations. Remaining data is merged from persistance automatically.
 					//Order is determinted by the order of array elements
-					if(IPS_ObjectExists($AlexaRow->instanceID)) {
+					if(IPS_ObjectExists($treeRow->instanceID)) {
 						$data->elements[0]->values[] = Array(
-							"name" => IPS_GetName($AlexaRow->instanceID),
+							"name" => IPS_GetName($treeRow->instanceID),
 							"state" => "OK!"
 						);
 					} else {
@@ -165,7 +166,7 @@
 			
 			return json_encode($data);
 		
-		}
+		}	
 		
 		public function MessageSink ($TimeStamp, $SenderID, $Message, $Data) {
 			
